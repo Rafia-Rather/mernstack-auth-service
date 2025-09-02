e0ef9623962854bff2ebd9630282ee65dd9e2957
Rafia-Rather <rafiarather24@gmail.com>
2025-09-02 19:12:20 +0530

    test lintstaged 5

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,7 @@
 function welcome(name: string) {
     console.log('welcome ' + name)
 
-    console.log('hello world')
-    const user = {
-        name: 'Rafia',
-    }
-
-    const fname = user.name
-    return name + fname
+    return name
 }
 
 welcome('Rafia')
